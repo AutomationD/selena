ebee0783c5cc7213bb509ba7b0d94bbf9674de9f
Mikhail Murzin <mezomish@gmail.com>
2014-07-11 19:50:49 -0400

    Fixed cross-domain requests

--- modules/modulebase.py
+++ modules/modulebase.py
@@ -7,6 +7,7 @@ class ModuleBase(object) :
 
     def GET(self, method='', **kwarg) :
         try :
+            cherrypy.response.headers['Access-Control-Allow-Origin'] = '*'
             return getattr(self, 'GET_' + method)()
         except AttributeError as e :
             cherrypy.response.status = 501
@@ -14,6 +15,7 @@ class ModuleBase(object) :
 
     def POST(self, method='', **kwarg) :
         try :
+            cherrypy.response.headers['Access-Control-Allow-Origin'] = '*'
             return getattr(self, 'POST_' + method)()
         except AttributeError as e :
             cherrypy.response.status = 501
