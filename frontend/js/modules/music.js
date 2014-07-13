203de4ffabcecb67adab556bd3a16d3febcd1452
Mikhail Murzin <mezomish@gmail.com>
2014-07-13 15:58:22 -0400

    Added requesting real data from Alice

--- frontend/js/modules/music.js
+++ frontend/js/modules/music.js
@@ -2,13 +2,26 @@ function getModule() {
 	return {
 		name: 'music',
 		title: "Music",
+		base_url: '',
 
 		content: function() {
-			return "<div class='module_content' id='music_content'>Music is playing</div>";
+			return "<div class='module_content' id='music_content'></div>";
 		},
 
 		start : function() {
+			this.getNowPlaying();
+		},
 
+		getNowPlaying: function() {
+			$.get( this.base_url + '/music/now_playing')
+				.done(
+					function( data ) {
+						$("#music_content").html( data );
+					}
+				)
+			;
 		}
+
+
 	}
 }
--- frontend/js/modules/weather.js
+++ frontend/js/modules/weather.js
@@ -2,13 +2,42 @@ function getModule() {
 	return {
 		name: 'weather',
 		title: "Weather",
+		base_url: '',
 
 		content: function() {
-			return "<div class='module_content' id='weather_content'>Weather is good</div>";
+			return "<div class='module_content' id='weather_content'>" +
+			"<div id='temperature'></div><br />" +
+			"<div id='weather'></div>" +
+			"</div>";
 		},
 
 		start : function() {
+			this.getWeather();
+		},
 
+
+		formatTemp : function(temperature) {
+			if ( temperature == '--' ) {
+				return '-- &deg;C';
+			}
+			var temp = Math.round( temperature * 10 ) / 10;
+			return temp.toFixed(1) + ' &deg;C';
+		},
+
+		getWeather: function() {
+
+			var weatherModule = this;
+			$.get( this.base_url + '/weather/current')
+				.done(
+					function( data ) {
+						var obj = jQuery.parseJSON(data);
+						$("#temperature").html( weatherModule.formatTemp(obj.temp) );
+						$("#weather").html(obj.weather.descr);
+					}
+				)
+			;
 		}
+
+
 	}
 }
